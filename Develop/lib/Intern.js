098efb819593f01d993c484a364fa105a972adec
Jalen Lewis <jalenlew1995@gmail.com>
2020-09-11 19:30:58 -0500

    More Changes

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+    constructor(id, name, email, school) {
+        super(id, name, email);
+        
+        this.school = school;
+    }
+
+    School() {
+        return this.school;
+    }
+
+    Position() {
+        return "Intern";
+    }
+}
+
+module.exports = Intern;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(id, name, email, officeNum) {
+        super(id, name, email);
+        
+        this.officeNum = officeNum;
+    }
+
+    OfficeNumber() {
+        return this.officeNum;
+    }
+
+    Position() {
+        return "Manager";
+    }
+}
+
+module.exports = Manager;
